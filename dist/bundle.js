cf588a17a24d60f847756073a2c6d6c085df5c83
tburakonat <tburakonat@gmail.com>
2022-08-03 17:18:31 +0200

    finished hardcoded restaurant page

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hello, there')
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ0hlbGxvLCB0aGVyZScpXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
